74a64eaae930e6af892277195409addfb017789c
camilounimilitar <141367253+camilounimilitar@users.noreply.github.com>
2023-09-06 21:18:44 -0500

    Add files via upload

---
+++ labs/02/Calculadora_Matrices/src/main/java/com/mycompany/calculadora_matrices/Lectura.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora_matrices;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ALEJANDRO
+ */
+public class Lectura {
+
+    static int tam1;
+
+    static Scanner in = new Scanner(System.in);
+
+    public static int[][] matriz_cuadrada(int a) {
+
+        System.out.println("ingrese el numero de filas de la matriz");
+        tam1 = in.nextInt();
+
+        int matrizA1[][] = new int[tam1][tam1];
+
+        for (int i = 0; i < tam1; i++) {
+            for (int j = 0; j < tam1; j++) {
+
+                System.out.println("Ingrese un numero para la matriz A:");
+                matrizA1[i][j] = in.nextInt();
+
+            }
+
+        }
+        return matrizA1;
+    }
+
+}
