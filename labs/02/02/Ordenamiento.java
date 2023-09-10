7cb6a4fac1704d60a24508bcb4437e650faf3613
camilounimilitar <141367253+camilounimilitar@users.noreply.github.com>
2023-09-10 12:20:56 -0500

    Add files via upload

---
+++ labs/02/02/Ordenamiento.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ALEJANDRO
+ */
+public class Ordenamiento {
+
+    public double[] burbuja(int a, double p[]) {
+        double aux;
+        double temporal[] = new double[a];
+        for (int i = 0; i < a - 1; i++) {
+            for (int j = 0; j < a - 1; j++) {
+                if (p[j] > p[j + 1]) {
+                    aux = p[j];
+
+                }
+            }
+        }
+
+    }
+
+}
